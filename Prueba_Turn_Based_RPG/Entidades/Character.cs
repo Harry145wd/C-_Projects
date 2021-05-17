f2b1c72741637f52a153bc1eafe878844ed4361b
Harry Martin <harrymartin145wd@hotmail.com>
2021-05-16 23:30:35 -0300

    Primer Commit Prueba_Turn_Based_RPG

---
+++ Prueba_Turn_Based_RPG/Entidades/Character.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Character
+    {
+        #region Atributes
+
+        protected string name;
+        protected int lifePoints;
+
+        protected double ATKBase;
+        protected double DEFBase;
+        protected double AGIBase;
+        protected double LCKBase;
+        protected double MAGBase;
+        protected double REGBase;
+
+        #endregion
+
+        #region Properties
+
+
+        #endregion
+
+        #region Constructors
+
+        public Character(string name, int lifePoints)
+        {
+            this.name = name;
+            this.lifePoints = lifePoints;
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected string Attack(object target, int attackType)
+        {
+            return "";
+        }
+
+
+        #endregion
+
+        #region Operators
+
+
+        #endregion
+
+    }
+}
---
+++ Prueba_Turn_Based_RPG/Entidades/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Enemy : Character
+    {
+
+
+        public Enemy(string name, int lifePoints) : base(name, lifePoints)
+        {
+
+        }
+    }
+}
